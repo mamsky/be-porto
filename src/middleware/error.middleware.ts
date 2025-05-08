412c9af5627de9a1ed26ce0a697f9e6d0162a954
Paste Prosmana <pasteprosmana189@gmail.com>
2025-05-08 12:52:26 +0700

    fix: error handler

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,17 +1,29 @@
-import { Request, Response, NextFunction } from "express";
+import { Prisma } from "@prisma/client";
+import { NextFunction, Request, Response } from "express";
+import Joi from "joi";
 
-export interface AppError extends Error {
-  status?: number;
-}
-
-export const ErrorHandler = (
-  err: AppError,
+export function errorHandler(
+  err: Error,
   req: Request,
   res: Response,
   next: NextFunction
-) => {
-  res.status(err.status || 500).json({
-    message: err.message || "Internal Server Error",
-  });
-  console.log(err.message);
-};
+) {
+  if (err instanceof Joi.ValidationError) {
+    res.status(400).json({
+      message: err.details[0].message,
+    });
+    return;
+  }
+
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    const { message } = err;
+    res.status(400).json({
+      message: message || "Terjadi kesalahan pada database.",
+    });
+    return;
+  }
+
+  res
+    .status(500)
+    .json({ message: `Internal Server Error! Error: ${JSON.stringify(err)}` });
+}
